a3a1cb797502c831dcaaab5fc5c96ef3d86b85ba
Prasadmts <prasadmts2507@gmail.com>
2023-08-02 21:38:30 +0530

    submitting code-Wed Aug 02 2023 21:38:23 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
+
+    
     <div id=" main">
       <p>I am learning React. My life is getting better.</p>
     </div>
-  )
+  );
 }
 
 
